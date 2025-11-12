967917cfc49d8e20155f09db846d5ad7982017f7
Giovaniolivr <giovaniolivr@gmail.com>
2025-11-11 22:32:47 -0300

    Criacao da tela de cadastro

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('cadastro/candidato/', views.register_candidate, name='register_candidate'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def register_candidate(request):
+    return render(request, 'register_candidate.html', {'hide_nav_links': True})
